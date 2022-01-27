4f1db915ef202ba4c67cf800d8c7e5264f348b4b
ebunoluwa56 <67189965+ebunoluwa56@users.noreply.github.com>
2022-01-27 01:28:56 +0100

    Added the bottomSheet and displayed the device location on the map

--- app/src/main/java/com/iyanuoluwa/fieldmaxproui/ScheduleActivity.kt
+++ app/src/main/java/com/iyanuoluwa/fieldmaxproui/ScheduleActivity.kt
@@ -1,49 +1,112 @@
 package com.iyanuoluwa.fieldmaxproui
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationRequest
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
 import android.widget.Toast
 import android.widget.Toast.*
+import androidx.annotation.NonNull
+import androidx.constraintlayout.motion.widget.Key.VISIBILITY
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.android.gms.tasks.Task
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionDeniedResponse
 import com.karumi.dexter.listener.PermissionGrantedResponse
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.single.PermissionListener
+import java.lang.Exception
 import java.util.jar.Manifest
 
-class ScheduleActivity : AppCompatActivity() {
-    var isPermissionGranted : Boolean = false
+class ScheduleActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var mapFragment : SupportMapFragment
+    private lateinit var lastLocation: Location
+    private lateinit var mMap : GoogleMap
+
+    companion object {
+        private const val LOCATION_REQUEST_CODE = 1
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_schedule)
-        checkPermissions()
+        val floatingActionButton = findViewById<FloatingActionButton>(R.id.fab)
+        val bottomSheetBehavior = BottomSheetBehavior.from(findViewById(R.id.sheet)).apply {
+            peekHeight = 250
+            this.state = BottomSheetBehavior.STATE_COLLAPSED
+        }
+        bottomSheetBehavior.setBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                if (newState == BottomSheetBehavior.STATE_EXPANDED) {
+                    floatingActionButton.visibility = View.GONE
+                } else if (newState == BottomSheetBehavior.STATE_COLLAPSED) {
+                    floatingActionButton.visibility = View.VISIBLE
+                }
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+
+            }
+        })
+
+        mapFragment= supportFragmentManager.findFragmentById(R.id.maps) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
     }
 
-    private fun checkPermissions() {
-        Dexter.withContext(this).withPermission(android.Manifest.permission.ACCESS_FINE_LOCATION).withListener(object : PermissionListener {
-            override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
-                isPermissionGranted = true
-                makeText(this@ScheduleActivity, "Permission Granted", LENGTH_SHORT).show()
-            }
-
-            override fun onPermissionDenied(p0: PermissionDeniedResponse?) {
-                val intent = Intent()
-                intent.action = Settings.ACTION_APPLICATION_SETTINGS
-                val uri = Uri.fromParts("package", packageName, "")
-                intent.data = uri
-                startActivity(intent)
-            }
-
-            override fun onPermissionRationaleShouldBeShown(
-                p0: PermissionRequest?,
-                p1: PermissionToken?
-            ) {
-                p1?.continuePermissionRequest()
-            }
-        }).check()
+    override fun onMapReady(p0: GoogleMap) {
+        mMap = p0
+        mMap.uiSettings.isZoomControlsEnabled = true
+        setUpMap()
     }
+
+    private fun setUpMap() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_REQUEST_CODE)
+            return
+        }
+        mMap.isMyLocationEnabled = true
+        fusedLocationProviderClient.lastLocation.addOnSuccessListener(this) { location ->
+            if (location != null) {
+                lastLocation = location
+                val currentLocation = LatLng(location.latitude, location.longitude)
+                placeMarkerOnMap(currentLocation)
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 16F))
+            }
+        }
+    }
+
+    private fun placeMarkerOnMap(currentLocation: LatLng) {
+
+        val markerOptions = MarkerOptions().position(currentLocation)
+        markerOptions.title("you are here")
+        mMap.addMarker(markerOptions)
+
+    }
+
 }
